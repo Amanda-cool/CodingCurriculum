6495f8151b7dcb2a5544c1aa49c10161242a1473
Amanda Davis <amdavis@redventures.com>
2020-07-13 10:59:38 -0400

    detail psge

--- 01_Core/week04/ecommerce/Vintage Vinyl Website/home.js
+++ 01_Core/week04/ecommerce/Vintage Vinyl Website/home.js
@@ -7,11 +7,13 @@ function carousel() {
   var i;
   var x = document.getElementsByClassName("mySlides");
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+    x[i].style.display = "none";
   }
   myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
+  if (myIndex > x.length) {
+    myIndex = 1
+  }
+  x[myIndex - 1].style.display = "block";
   setTimeout(carousel, 2000); // Change image every 2 seconds
 }
 
@@ -26,3 +28,21 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+// Products Details Page Quantity
+/*
+function increaseValue() {
+  var value = parseInt(document.getElementById('number').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value++;
+  document.getElementById('number').value = value;
+}
+
+function decreaseValue() {
+  var value = parseInt(document.getElementById('number').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value < 1 ? value = 1 : '';
+  value--;
+  document.getElementById('number').value = value;
+}
+*/
