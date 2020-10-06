3dfd99349f0e41a601b4c0443670d8cc53b5d35b
Amanda Davis <amdavis@redventures.com>
2020-10-06 14:21:55 -0400

    hallllp

--- 01_Core/week04/ecommerce/Vintage Vinyl Website/front_end/assets/js/carouselHome.js
+++ 01_Core/week04/ecommerce/Vintage Vinyl Website/front_end/assets/js/carouselHome.js
@@ -1,7 +1,25 @@
-//Slideshow Carousel Main Page
+var prodContainer = document.querySelector(".featured");
+fetch(`http://localhost:2000/genres`)
+    // Decoding json back to an array of objects, from the struct created in go
+    .then(response => {
+        return response.json()
+    })
+    //looping through the previous array to return the data in innerHTML
+    .then(data => {
+      prodContainer.innerHTML +=`
+      <h2>Featured Artist:</h2>        
+      <img class="mySlides" alt="${data[7].artist}" src="${data[7].img}">
+      <img class="mySlides" alt="${data[5].artist}" src="${data[5].img}">
+      <img class="mySlides" alt="${data[6].artist}" src="${data[6].img}">
+      <img class="mySlides" alt="Neutral Milk Hotel" src="assets/images/neutral_milk_hotel.jpg">
+      <img class="mySlides" alt="Ugly Casanova" src="assets/images/ugly_casanova.jpg">
+`
+    })
 
+
+//Slideshow Carousel Main Page
 var myIndex = 0;
-carousel();
+setTimeout(carousel, 100); 
 
 function carousel() {
   var i;
@@ -16,3 +34,4 @@ function carousel() {
   x[myIndex - 1].style.display = "block";
   setTimeout(carousel, 2000); // Change image every 2 seconds
 }
+
--- 01_Core/week04/ecommerce/Vintage Vinyl Website/front_end/assets/js/genres.js
+++ 01_Core/week04/ecommerce/Vintage Vinyl Website/front_end/assets/js/genres.js
@@ -1,19 +1,7 @@
-// var prodData=[];
-// async function getProducts(){
-//     const response = await fetch("http://localhost:2000/genres", {
-
-
-//     })
-//     console.log(response);
-//     prodData = await response.json();
-//     console.log(prodData);
-// };
-// getProducts();
-
-
 // Fetching products from database and applying to HTML:
 // creating variable to utilize later for querySelector on record-container to tell it where I want to insert HTML
 var prodContainer = document.querySelector(".record-container")
+var dataArr = [];
 //Fetching products utilizing genres endpoint created in go
 fetch(`http://localhost:2000/genres`)
     // Decoding json back to an array of objects, from the struct created in go
@@ -28,16 +16,15 @@ fetch(`http://localhost:2000/genres`)
                 <a href="detail.html">
                     <img src="${data[i].img}" alt="${data[i].artist}" class="record_ind">
                     <p class="record_pad">${data[i].artist}</p>
-                    <p>${data[i].price}</p>
+                    <p class="price-magic">$${data[i].price}</p>
                     <p class="genre-magic">${data[i].genre}</p>
                 </a>
             </div>`
         }
+        return dataArr = data;
     })
 
 // Search functionality
-
-
 function search() {
     const indProd = document.querySelectorAll(".ind-prod");
     let input = document.getElementById("search-box");
@@ -55,12 +42,12 @@ function search() {
     }
 }
 // Filter functionality for genre
-
 function sortGenre() {
     const indProd = document.querySelectorAll(".ind-prod");
     let genreInput = document.getElementById("genres").value;
 
     for (let i = 0; i < indProd.length; i++) {
+//
         let genreArr = indProd[i].getElementsByClassName("genre-magic")  [0];
         let valGen = genreArr.textContent;
 
@@ -77,5 +64,37 @@ function sortGenre() {
 
 
 
+// Sort functionality for price
+function sortPrice() {
+    prodContainer.innerHTML = ""
+    let sortSelect = document.getElementById("price").value;
+//
+    if(sortSelect === "Highest") {
+        dataArr.sort((a, b) => (a.price < b.price)? 1 : -1);
+    }  else if(sortSelect === "Lowest") {
+        dataArr.sort((a, b) => (a.price > b.price)? 1 : -1);
+    }
+//
+    for (i = 0; i < dataArr.length; i++) {
+        prodContainer.innerHTML += `
+        <div class="ind-prod">
+            <a href="detail.html">
+                <img src="${dataArr[i].img}" alt="${dataArr[i].artist}" class="record_ind">
+                <p class="record_pad">${dataArr[i].artist}</p>
+                <p class="price-magic">$${dataArr[i].price}</p>
+                <p class="genre-magic">${dataArr[i].genre}</p>
+            </a>
+        </div>`
+    }
+}
 
-// Filter functionality for price
+
+
+
+
+
+
+
+
+
+
